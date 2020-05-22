821f26e4ba51a3cab8b25cc9f69043248017dca7
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2020-05-22 22:21:13 +0530

    implement H2 database and service layer

--- server/user/src/main/java/com/miniinsta/user/UserController.java
+++ server/user/src/main/java/com/miniinsta/user/UserController.java
@@ -1,6 +1,13 @@
 package com.miniinsta.user;
 
 import com.miniinsta.user.Model.CreateUserRequestModel;
+import com.miniinsta.user.service.UserService;
+import com.miniinsta.user.shared.UserDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -8,14 +15,19 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-
+    @Autowired
+    UserService userService;
     @GetMapping("check")
     public String getHealthCheck() {
         return "User API Services are UP";
     }
 
     @PostMapping("/createUser")
-    public String createUser (@Valid @RequestBody CreateUserRequestModel user) {
-        return "user";
+    public ResponseEntity createUser (@Valid @RequestBody CreateUserRequestModel userDetails) {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+        userService.createUser(userDto);
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
--- server/user/src/main/java/com/miniinsta/user/service/UserServiceImpl.java
+++ server/user/src/main/java/com/miniinsta/user/service/UserServiceImpl.java
@@ -1,15 +1,32 @@
 package com.miniinsta.user.service;
 
+import com.miniinsta.user.data.UserEntity;
+import com.miniinsta.user.data.UserRepository;
 import com.miniinsta.user.shared.UserDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
-
+@Service
 public class UserServiceImpl implements UserService {
+    UserRepository userRepository;
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDto createUser(UserDto userDetails) {
         userDetails.setUserId(UUID.randomUUID().toString());
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+        userEntity.setEncryptedPassword("test");
+        userRepository.save(userEntity);
         return null;
     }
 }
